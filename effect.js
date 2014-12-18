192fe58f12d19ff0e6d565524a7a9cf999e2ef46
Shohei Aoki <shoaok@gmail.com>
2014-12-19 01:13:51 +0900

    save

---
+++ effect.js
@@ -0,0 +1,19 @@
+$(function(){
+    // $('button').click(function() {
+    //     $(this).effect( "highlight", {color: 'red'}, 3000 );
+    // });
+    // $("#loadpng").effect("highlight",{color:'orange'},3000);
+    $("#sendrml").effect("highlight",{color:'orange'},3000);
+    // $("#makepath").effect("highlight",{color:'orange'},3000);
+    $("#makerml").effect("highlight",{color:'orange'},3000);
+
+// $("makepath").click(function() {
+  // do fading 3 times
+  for(i=0;i<10;i++) {
+    $("#makepath").effect("highlight",{color:'orange'},3000);
+    // $("#makepath").effect("highlight",{color:'orange'},3000);
+    // $(this).fadeTo('slow', 0.5).fadeTo('slow', 1.0);
+  }
+
+
+})
